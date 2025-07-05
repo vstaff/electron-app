75a0858dbee2ab3898f71c103bf2fdf86abc55f9
vstaff <vstaff22b@gmail.com>
2025-07-05 12:17:45 +1000

    MyDND принимает максимум один файл

--- src/components/MyDND.tsx
+++ src/components/MyDND.tsx
@@ -3,27 +3,47 @@ import { useCallback } from "react";
 import { useDropzone } from "react-dropzone/.";
 
 export default function MyDND() {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps
+  } = useDropzone({    
+    maxFiles: 1
+  });
 
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-      // Do whatever you want with the file contents
-        const binaryStr = reader.result
-        console.log(binaryStr)
-      }
-      reader.readAsArrayBuffer(file)
-    })
-    
-  }, [])
-  const {getRootProps, getInputProps} = useDropzone({onDrop})
+  const acceptedFileItems = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors  }) => { 
+   return (
+     <li key={file.path}>
+          {file.path} - {file.size} bytes
+          <ul>
+            {errors.map(e => <li key={e.code}>{e.message}</li>)}
+         </ul>
+
+     </li>
+   ) 
+  });
+  
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </div>
-  )
+    <section className="container">
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+        <em>(2 files are the maximum number of files you can drop here)</em>
+      </div>
+      <aside>
+        <h4>Accepted files</h4>
+        <ul>{acceptedFileItems}</ul>
+        <h4>Rejected files</h4>
+        <ul>{fileRejectionItems}</ul>
+      </aside>
+    </section>
+  );
 }
